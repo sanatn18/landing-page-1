0d65cf1c2f20330a2a3b7ad79172131269044eeb
SanatNaik <sanatn2004@gmail.com>
2024-09-24 20:37:33 +0530

    added hamburger menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,29 @@
+import React, { useState } from 'react';
 import './Navbar.css'
 
 const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false); //creating a state for the hamburger menu
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className='nav'>
         <div className="nav-logo">Bikers Arena</div>
-        <ul className="nav-menu">
-            <li>Home</li>
-            <li>Explore</li>
-            <li>About</li>
-            <li className='nav-contact'>Contact</li>
-        </ul>
+        <div className={`nav-menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+      </div>
+      
+      <ul className={`nav-menu ${menuOpen ? 'active' : ''}`}>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>About</li>
+        <li className='nav-contact'>Contact</li>
+      </ul>
     </div>
   )
 }
